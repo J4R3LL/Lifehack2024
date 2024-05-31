4b6c1caaaa284b31dbaebd8e22aa11d882d88438
J4R3LL <jjl.phang06@gmail.com>
2024-06-01 01:15:22 +0800

    returns cna articles

--- articles_jarell.py
+++ articles_jarell.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 def scrape_links(url):
     try:
@@ -27,10 +28,49 @@ def scrape_links(url):
         print(f"An error occurred: {e}")
         return []
 
-url = 'https://www.channelnewsasia.com/topic/terrorism?sort_by=field_release_date_value&sort_order=DESC&page=1'  # Replace with the target URL
-links = scrape_links(url)
-#get individual article links
-links = [link for link in links if link.startswith('/singapore')]
+def scrape_article(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'html.parser')
+        main_content = soup.find('section', id='block-mc-cna-theme-mainpagecontent')
+        if main_content:
+            first_child = main_content.findChild()
+            if first_child:
+                content_child = first_child.find('div', class_='content')
+                if content_child:
+                    last_child = content_child.find_all(recursive=False)[3]
+                    first_child = last_child.findChild()
+                    first_child = first_child.findChild()
+                    if first_child:
+                        child_div = first_child.find('div')
+                        if child_div:
+                            contents = ''
+                            contents = contents.join([re.sub(r'[\xa0/\'"]', '', paragraph.get_text(separator=' ', strip=True)) for paragraph in child_div.find_all('p')])
+                            
+                            return contents
+
+        return {"first_child_texts": "No content found", "last_child_texts": "No content found"}
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return {"first_child_texts": "Error fetching the article", "last_child_texts": "Error fetching the article"}
 
 
-print("Found links:", links)
+def cna_articles():
+    base_url = 'https://www.channelnewsasia.com'
+    articles = []
+    for i in range(7):
+        url = f'https://www.channelnewsasia.com/topic/terrorism?sort_by=field_release_date_value&sort_order=DESC&page={i}'  # Replace with the target URL
+        links = scrape_links(url)
+        #get individual article links
+        filtered_links = [link for link in links if link.startswith('/singapore')]
+
+        for link in filtered_links:
+            full_url = base_url + link  # Construct full URL
+            article = scrape_article(full_url)
+            articles.append(article)
+    return articles
+
+if __name__ == '__main__':
+    print(cna_articles()[:3])
+    print(len(cna_articles()))
